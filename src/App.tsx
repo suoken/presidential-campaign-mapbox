1dc42f1c37116330a58e7fcedd35cb1a8a33b4a6
suoken <kensuong@gmail.com>
2023-06-13 17:51:04 -0700

    feat: ControlPanel added with text

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
+import React from 'react'
 import Map, { GeolocateControl, FullscreenControl, NavigationControl, ScaleControl } from 'react-map-gl'
+import ControlPanel from './components/ControlPanel'
 import './App.css'
 
-const TOKEN = 'pk.eyJ1Ijoic3Vva2VuIiwiYSI6ImNsaXA2aDlubzAzZ3czZHF2N2JweW1mejMifQ.oTJy_GGp_vg0F_bt1oWzHA';
+const TOKEN = 'pk.eyJ1Ijoic3Vva2VuIiwiYSI6ImNsaXA2aDlubzAzZ3czZHF2N2JweW1mejMifQ.oTJy_GGp_vg0F_bt1oWzHA'
 
-function App() {
+// sets up a good zoom to see the entire USA 
+const USA_INITIAL_VIEW_STATE = {
+  latitude: 40,
+  longitude: -100,
+  zoom: 4.0,
+  bearing: 0,
+  pitch: 0
+}
 
+const App:React.FC = () => {
   return (
-    <Map
-      initialViewState={{
-        latitude: 40,
-        longitude: -100,
-        zoom: 3.5,
-        bearing: 0,
-        pitch: 0
-      }}
-      mapStyle='mapbox://styles/mapbox/streets-v12'
-      mapboxAccessToken={TOKEN}
-    >
-      <GeolocateControl position="top-left" />
-      <FullscreenControl position="top-left" />
-      <NavigationControl position="top-left" />
-      <ScaleControl />
-    </Map>
+    <>
+      <Map
+        initialViewState = { USA_INITIAL_VIEW_STATE }
+        mapStyle = 'mapbox://styles/mapbox/streets-v12'
+        mapboxAccessToken = { TOKEN }
+      >      
+        <GeolocateControl position = "top-left" />
+        <FullscreenControl position = "top-left" />
+        <NavigationControl position = "top-left" />
+        <ScaleControl />
+      </Map>
+      <ControlPanel />
+    </>
   )
 }
 
---
+++ src/components/ControlPanel.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const ControlPanel:React.FC = () => {
+    return (
+        <div className="control-panel">
+            <h3>Presidental Campaign Trail</h3>
+            <p>Map showing the short distance to travel to each city</p>
+        </div>
+    )
+}
+
+export default ControlPanel
